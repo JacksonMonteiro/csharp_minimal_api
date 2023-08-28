d3748b1fea8a436486102e43163d2c856e7b4935
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-08-28 15:33:01 -0300

    Task class, DB Context class, Get and create tasks methods created

--- TaskAPI/Program.cs
+++ TaskAPI/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TaskAPIContext>(options =>
+    options.UseInMemoryDatabase("TaskDB")
+    );
 
 var app = builder.Build();
 
@@ -13,29 +17,32 @@ if (app.Environment.IsDevelopment()) {
     app.UseSwaggerUI();
 }
 
+
+// Tasks Endpoint
+app.MapGet("/tasks", async (TaskAPIContext db) => {
+    return await db.Tasks.ToListAsync();
+});
+
+app.MapPost("/tasks", async (Task task, TaskAPIContext db) => {
+    db.Tasks.Add(task);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tasks/{task.Id}", task);
+});
+
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () => {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// Classes
+class Task {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool? isFinished { get; set; }
+}
+
+class TaskAPIContext : DbContext {
+    public TaskAPIContext(DbContextOptions<TaskAPIContext> options) : base(options) {
+
+    }
+
+    public DbSet<Task> Tasks => Set<Task>();
 }
